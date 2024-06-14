715b8f427feeb6acaa3be6d521be1b586735287f
iasonasxrist <iasonasxrist@gmail.com>
2024-06-14 11:57:11 +0300

    CircularList Update

--- cache-gateway/CircularLinkedList.py
+++ cache-gateway/CircularLinkedList.py
@@ -3,39 +3,33 @@ class Node:
         self.data = data
         self.next = None
 
-
 class CircularLinkedList:
     def __init__(self):
         self.head = None
-    
+        self.current = None
+
     def append(self, data):
         new_node = Node(data)
-        if self.head is None:
+        if not self.head:
             self.head = new_node
             self.head.next = self.head
         else:
             temp = self.head
-            while (temp.next != self.head):
+            while temp.next != self.head:
                 temp = temp.next
-            
             temp.next = new_node
             new_node.next = self.head
 
-    def get_next_node(self, current_node):
-        if current_node is None:
-            return None
-        next_node = current_node.next
-        return next_node
-
-    def first_node(self):
-        return self.head
+    def getNext(self):
+        if not self.current:
+            self.current = self.head
+        else:
+            self.current = self.current.next
+        if self.current:
+            return self.current.data
+        return None
     
-# c =  CircularLinkedList()
-# c.append(1)
-# c.append(2)
-# c.append(3)
-# a = c.first_node()
-# b = c.get_next_node(a)
-# print(b.data)
-# d = c.get_next_node(b)
-# print(d.data)
+    
+    def clear(self):
+        self.head = None
+        self.current = None
--- cache-gateway/main_test.py
+++ cache-gateway/main_test.py
@@ -2,60 +2,43 @@ import unittest
 from CircularLinkedList import CircularLinkedList
 
 class TestCircularLinkedList(unittest.TestCase):
+
     def setUp(self):
         self.cll = CircularLinkedList()
-
-    def test_append_to_empty_list(self):
+        
+    def test_append_single_element(self):
         self.cll.append(1)
-        self.assertIsNotNone(self.cll.head)
         self.assertEqual(self.cll.head.data, 1)
         self.assertEqual(self.cll.head.next, self.cll.head)
-
+        
     def test_append_multiple_elements(self):
         self.cll.append(1)
         self.cll.append(2)
         self.cll.append(3)
-        
         self.assertEqual(self.cll.head.data, 1)
         self.assertEqual(self.cll.head.next.data, 2)
         self.assertEqual(self.cll.head.next.next.data, 3)
         self.assertEqual(self.cll.head.next.next.next, self.cll.head)
-        
-    def test_get_next_node(self):
+
+    def test_get_next(self):
         self.cll.append(1)
         self.cll.append(2)
         self.cll.append(3)
+        self.assertEqual(self.cll.getNext(), 1)
+        self.assertEqual(self.cll.getNext(), 2)
+        self.assertEqual(self.cll.getNext(), 3)
+        self.assertEqual(self.cll.getNext(), 1)
         
-        current = self.cll.head
-        self.assertEqual(current.data, 1)
-        next_node = self.cll.get_next_node(current)
-        self.assertEqual(next_node.data, 2)
-
-        current = next_node
-        next_node = self.cll.get_next_node(current)
-        self.assertEqual(next_node.data, 3)
-
-        current = next_node
-        next_node = self.cll.get_next_node(current)
-        self.assertEqual(next_node.data, 1)
-
-    def test_get_next_node_single_element(self):
+    def test_clear(self):
         self.cll.append(1)
-        current = self.cll.head
-        next_node = self.cll.get_next_node(current)
-        self.assertEqual(next_node.data, 1)
-        self.assertEqual(next_node, self.cll.head)
-    
-    def test_get_next_node_none(self):
-        self.assertIsNone(self.cll.get_next_node(None))
-
-    def test_first_node(self):
-        self.assertIsNone(self.cll.first_node())
-        
-        self.cll.append(1)
-        self.assertEqual(self.cll.first_node().data, 1)
         self.cll.append(2)
-        self.assertEqual(self.cll.first_node().data, 1)
+        self.cll.append(3)
+        self.cll.clear()
+        self.assertIsNone(self.cll.head)
+        self.assertIsNone(self.cll.current)
+        
+    def test_get_next_on_empty_list(self):
+        self.assertIsNone(self.cll.getNext())
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
